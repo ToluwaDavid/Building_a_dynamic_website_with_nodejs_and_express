80bc94f28e81805147c9aabcef39d01602186d05
ToluwaDavid <32324558+ToluwaDavid@users.noreply.github.com>
2021-12-31 17:55:13 +0100

    Add files via upload

---
+++ building-website-nodejs-express-copy/server.js
@@ -0,0 +1,78 @@
+const { response } = require('express');
+const express = require('express');
+const { request } = require('http');
+const path = require('path');
+const cookieSession = require('cookie-session');
+const createError = require('http-errors');
+const bodyParser = require('body-parser');
+
+const FeedbackService = require('./services/FeedbackService');
+const SpeakersService = require('./services/SpeakerService');
+
+
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakersService = new SpeakersService('./data/speakers.json');
+const routes = require('./routes')
+
+const app = express();
+
+
+const port = 3000;
+
+app.set('trust proxy', 1)
+
+app.use(
+    cookieSession({
+        name: 'session',
+        keys: ['hhshshsnsjdnjswjhe', 'wuwhdbbskBAKGFBVbvbdjd']
+    })
+)
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
+app.locals.siteName = 'ROUX Meetups';
+
+app.use(express.static(path.join(__dirname, './static')));
+
+app.use(async (request, response, next) => {
+    try {
+        const names = await speakersService.getNames();
+        response.locals.speakerNames = names;
+        // console.log(response.locals);
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+})
+
+app.use('/', routes({ feedbackService, speakersService }));
+// app.get('/', (request, response) => {
+//     // response.send('Hello Express');
+//     // response.sendFile(path.join(__dirname, '/static/index.html'));
+//     response.render('pages/index', { pageTitle: 'Welcome' });
+
+// });
+
+// app.get('/speakers', (request, response) => {
+//     response.sendFile(path.join(__dirname, '/static/speakers.html')); s
+// })
+app.use((request, response, next) => {
+    return next(createError(404, 'File not found'))
+});
+
+app.use((err, request, response, next) => {
+    response.locals.message = err.message;
+    console.error(err);
+    const status = err.status || 500;
+    response.locals.status = status;
+    response.status(status);
+    response.render('error')
+})
+
+app.listen(port, () => {
+    console.log(`Express server is listening at port ${port}!`);
+})
